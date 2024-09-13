4737270ae8a260e2e19954c78e4da5f0f9cf8922
n.kovalev <n.kovalev@npo-echelon.ru>
2024-09-13 11:43:42 +0300

    WIP: first interfaces

---
+++ src/main.py
@@ -0,0 +1,18 @@
+
+def main():
+
+    print("Write name:")
+    name = input()
+    print("Write color:")
+    color =  input()
+
+
+
+def CountPriceOfPainting(name, color): 
+    """return price"""
+
+def CalculateColor(): 
+    """return color"""
+
+def CalculateDetail():
+    """return price""" 
