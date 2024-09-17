2aa817546067961e36ffe8f903fbecd559dc9fcf
n.kovalev <n.kovalev@npo-echelon.ru>
2024-09-17 11:03:25 +0300

    deleted the extra folder

--- src/main.py
+++ src/main.py
@@ -1,20 +1,27 @@
 
 def main():
-
-    print("Write name:")
-    name = input()
+    print("Write name of detail:")
+    detail = input()
     print("Write color:")
     color =  input()
+    total_cost = CountPriceOfPainting(detail, color)
+    print ("Total cost is " , total_cost)
 
 
 
-def CountPriceOfPainting(name, color): 
-    """return price"""
-
-def CalculateColor(): 
-    """return color"""
+def CalculateColor(color): 
+    colors = {'Белый': 1, 'Синий': 1, 'Желтый': 1.1, 'Красный': 1, 'Перламутровый': 1.2, 'Серый металлик': 1.3}
+    return colors[color]
 
 def CalculateDetail(detail):
-    """return ratio""" 
     details = {'Капот': 1, 'Передняя дверь': 1.2, 'Задняя дверь': 1.1, 'Передний бампер': 1, 'Задний бампер': 1, 'Крыша': 1.1}
     return details[detail]
+
+def CountPriceOfPainting(detail, color): 
+    cost_color = CalculateColor(color)
+    cost_detail = CalculateDetail(detail)
+    return (cost_color+cost_detail)*12000
+
+
+
+main()
